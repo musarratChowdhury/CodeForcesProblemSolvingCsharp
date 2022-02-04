bcc624f3bc9489d0290bc1c2a3f68f55df8d5610
musarratchowdhury <muhitlopez143@gmail.com>
2022-02-04 13:36:27 +0600

    boilerPlate code added

---
+++ BoilerPlateCode/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Text;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            var line1 = Console.ReadLine().Split(" ");
+            int n = Convert.ToInt32(line1[0]);
+            int m = Convert.ToInt32(line1[1]);
+            var line2 = Console.ReadLine().Split(" ");
+
+            //necessary variables
+
+
+            //solution
+
+           
+
+        }
+    }
+}
