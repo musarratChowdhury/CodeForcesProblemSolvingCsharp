8db77e8f0629a0de708721612ccee7ec597da284
musarratChowdhury <muhitlopez143@gmail.com>
2022-08-21 10:09:18 +0600

    up

---
+++ problem-1154-A-RestoringThreeSums/Program.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using static System.Console;
+using System.Text;
+using System.Linq;
+using System.Threading;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace problem
+{
+  class Program
+  {
+    static void Main( string[] args )
+    {
+			//input
+			var line1 = ReadLine()?.Split(" ");
+			int n = Convert.ToInt32(line1?[0]);
+			int m = Convert.ToInt32(line1?[1]);
+			int o = Convert.ToInt32(line1?[2]);
+			int p = Convert.ToInt32(line1?[3]);
+
+			var numbers = new List<int>();
+
+			for(int i = 0;i<line1.Length;i++)
+			{
+				numbers.Add(Convert.ToInt32(line1[i]));
+			}
+			numbers.Sort();
+			var additionOfThreeNumbers = numbers[3];
+			var anotherAddition = numbers[0];
+
+			var number1 = additionOfThreeNumbers-numbers[1];
+			var number2 = anotherAddition - number1;
+			var number3 = numbers[1] - number2;
+
+			Console.WriteLine($"{number1} {number2} {number3}");
+			//Console.WriteLine(additionOfThreeNumbers);
+
+
+
+
+
+			//Read();
+			//necessary variables
+
+
+			//solution
+
+
+
+		}
+	}
+}
---
+++ problem-1399-A-RemoveSmallest/Program.cs
@@ -0,0 +1,68 @@
+﻿
+using System;
+using static System.Console;
+using System.Text;
+using System.Linq;
+using System.Threading;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace problem
+{
+  class Program
+  {
+    static void Main( string[] args )
+    {
+			//input
+			var numberOfTestCases = Convert.ToInt32(ReadLine());
+			
+
+			while(numberOfTestCases > 0)
+			{
+				var testCaseArr = new List<int>();
+				var n = Convert.ToInt32(ReadLine());
+				var line1 = Console.ReadLine().Split(" ");
+				for(int i = 0;i<line1.Length;i++)
+				{
+					testCaseArr.Add(Convert.ToInt32(line1[i]));
+				}
+
+				//Console.WriteLine(testCaseArr[0]);
+				//check for sorted arr
+				if(CheckArr(testCaseArr))
+				{
+					Console.WriteLine("YES");
+				}
+				else
+				{
+					Console.WriteLine("NO");
+				}
+				numberOfTestCases--;
+			}
+
+
+
+			//Read();
+			//necessary variables
+
+
+			//solution
+
+
+
+		}
+		static bool CheckArr(List<int> arr)
+		{
+			arr.Sort();
+			for(int i = 0;i< arr.Count;i++)
+			{
+				if(i != arr.Count - 1&&arr[i+1]-arr[i]>1)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+  }
+}
