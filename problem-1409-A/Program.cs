2dd1906cd069ccbe0906352f291613f04c0164be
musarratChowdhury <muhitlopez143@gmail.com>
2022-08-25 17:11:57 +0600

    new

---
+++ problem-1409-A/Program.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+using static System.Console;
+using System.Text;
+using System.Linq;
+using System.Threading;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace problem
+{
+  class Program
+  {
+    static void Main( string[] args )
+    {
+			//input
+			var testCases = Convert.ToInt32(ReadLine());
+
+			while(testCases>0)
+			{
+				testCases--;
+				var line = Console.ReadLine().Split(" ");
+				long a = Convert.ToInt64(line[0]);
+				long b = Convert.ToInt64(line[1]);
+
+				long steps;
+
+				//if(a>b)
+				//{
+				//	//use reduction
+					var diff = Math.Abs(a - b);
+					 steps = diff / 10;
+					if(diff % 10 != 0)
+					{
+						steps++;
+					}
+				Console.WriteLine(steps);
+				
+				
+
+			}
+			//int n = Convert.ToInt32(line1?[0]);
+			//int m = Convert.ToInt32(line1?[1]);
+			//var line2 = ReadLine()?.Split(" ");
+
+
+
+			//Read();
+			//necessary variables
+
+
+			//solution
+
+
+
+		}
+  }
+}
---
+++ problem-723-A/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace problem_723_A
+{
+	internal class Program
+	{
+		static void Main( string[] args )
+		{
+			//Console.WriteLine("Hello, World!");
+			//input
+			var line = Console.ReadLine().Split(" ");
+			List<int> cords = new();
+
+			foreach (var i in line)
+			{
+				cords.Add(Convert.ToInt32(i));
+			}
+
+			cords.Sort();
+			int max = cords[2];
+			int middle = cords[1];
+			int min = cords[0];
+			int midpoint;
+			int A, B, C;
+
+
+			//if (Math.Abs(middle - max) > Math.Abs(middle - min))
+			//{
+				A = Math.Abs(middle - min);
+				B = 0;
+				C = Math.Abs(middle - max);
+				Console.WriteLine(A + B + C);
+
+			//}
+			//else
+			//{
+   //      //
+			//}
+
+			//int dmaxmin = max - min;
+			//double avgmaxmin = dmaxmin / 2;
+			//double probableMiddlePoint = min + avgmaxmin;
+
+			//if(probableMiddlePoint == (int)probableMiddlePoint)
+			//{
+			//	A = Math.Abs(middle - (int)probableMiddlePoint);
+			//	B = Math.Abs(max - (int)probableMiddlePoint);
+			//	C = Math.Abs(min - (int)probableMiddlePoint);
+			//	Console.WriteLine(A + B + C);
+			//	return;
+			//}
+			//else
+			//{
+
+			//	if((middle - (int)probableMiddlePoint)>(middle-Math.Ceiling(probableMiddlePoint)))
+			//	{
+			//		midpoint = (int)probableMiddlePoint;
+			//	}else
+			//	{
+			//		midpoint = (int)Math.Ceiling(probableMiddlePoint);
+			//	}
+			//	A = Math.Abs(middle - midpoint);
+			//	B = Math.Abs(max - midpoint);
+			//	C = Math.Abs(min - midpoint);
+			//	Console.WriteLine(A + B + C);
+			//	return;
+			//}
+
+		}
+	}
+}
